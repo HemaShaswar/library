f728323a6dbf06400f6acf9bcfd4e6c6bd474c5f
Hema <shaswarhema@gmail.com>
2023-01-14 14:26:43 +0300

    sloppy but functioning

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const author =document.querySelector('#author');
 const pages =document.querySelector('#pages');
 const read =document.querySelector('#read');
 const shelf = document.querySelector('.books');
+const showForm = document.querySelector('#form');
+const form = document.querySelector('.input');
+const close = document.querySelector('.close');
+const bg = document.querySelector('.bg');
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
     newBook();
+    bg.classList.add('closed')
+    form.classList.add('closed')
 });
 
 function Book(title, author, pages, read) {
@@ -16,18 +22,64 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+
 function newBook() {
     
     let book = new Book(title.value, author.value, pages.value, read.checked);
-    console.log(book)
     const addedBook = document.createElement('div');
     addedBook.classList.add('book')
     addedBook.innerText = 
     `Title: ${book.title} 
     Author: ${book.author}
-    Pages: ${book.pages}
-    Have read: ${book.read}`;
+    Pages: ${book.pages}`;
+
+    const removeBtn = document.createElement('button')
+    removeBtn.classList.add('remove');
+    removeBtn.innerText = 'remove';
+    addedBook.appendChild(removeBtn);
     shelf.appendChild(addedBook);
 
+    const readBtn = document.createElement('button')
+    readBtn.classList.add('remove');
+    if (book.read === false) {
+        readBtn.innerText = 'Not Read'
+    } else {
+        readBtn.innerText = 'Read'
+    }
+    
+    addedBook.appendChild(readBtn);
+    
+    readBtn.addEventListener('click', (e) => {
+        if (e.target.innerText === 'read') {
+            e.target.innerText = 'Not Read'
+        } else {
+            e.target.innerText = 'read'
+        }
+
+    })
+    
+    removeBtn.addEventListener('click', (e) => {
+        e.target.parentElement.remove()
+    })
+    
 }
+
+showForm.addEventListener('click', () => {
+    form.classList.remove('closed')
+    bg.classList.remove('closed')
+})
+
+close.addEventListener('click', () => {
+    bg.classList.add('closed')
+    form.classList.add('closed')
+
+})
+
+bg.addEventListener('click', (e) => {
+    if (e.target == bg) {
+        bg.classList.add('closed')
+        form.classList.add('closed')       
+    }
+})
 
